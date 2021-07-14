d202b5abe4241a4114a120fdf00e0a8be752fd24
helios <vergel>
2021-07-14 18:00:34 +1200

    initil commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import albumSliceReducer from '../features/albums/albumSlice';
+import photoSlice from '../features/photos/photoSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    albums: albumSliceReducer,
+    photos: photoSlice,
   },
 });
